ce034b9dd927dabd8b84c3fa24a3f76e8f9a2a40
HarleyVader <urukkai@outlook.com>
2025-02-03 17:56:55 +0100

    0.0.2

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const { Worker } = require('worker_threads');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const worker = new Worker(path.join(__dirname, 'workers', 'ultravox.js'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  
+  socket.on('message', (msg) => {
+    worker.postMessage(msg);
+  });
+
+  worker.on('message', (response) => {
+    socket.emit('message', response);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
